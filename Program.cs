382cb336dbef7ef02c251f4905c3595feb239604
Полина <goldibina@mail.ru>
2025-06-11 17:31:28 +0700

    Initial commit with basic menu structure

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information!
+
+using System;
+
+namespace ConsoleAppWithGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в многофункциональное консольное приложение!");
+            Console.WriteLine("Выберите функцию:");
+            Console.WriteLine("1. Калькулятор");
+            Console.WriteLine("2. Генератор случайных чисел");
+            Console.WriteLine("3. Проверка палиндрома");
+            Console.WriteLine("4. Конвертер температуры");
+            Console.WriteLine("0. Выход");
+            
+            bool running = true;
+            while (running)
+            {
+                Console.Write("\nВведите номер функции: ");
+                string input = Console.ReadLine();
+                
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Калькулятор - функция будет реализована");
+                        break;
+                    case "2":
+                        Console.WriteLine("Генератор случайных чисел - функция будет реализована");
+                        break;
+                    case "3":
+                        Console.WriteLine("Проверка палиндрома - функция будет реализована");
+                        break;
+                    case "4":
+                        Console.WriteLine("Конвертер температуры - функция будет реализована");
+                        break;
+                    case "0":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Неверный ввод. Попробуйте снова.");
+                        break;
+                }
+            }
+            
+            Console.WriteLine("Спасибо за использование приложения!");
+        }
+    }
+}
